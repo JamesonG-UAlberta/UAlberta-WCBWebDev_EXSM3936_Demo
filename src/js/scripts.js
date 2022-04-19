f006b1b88135eb726e392fdba4c1a4d87a1ad795
James Grieve <jgrieve@ualberta.ca>
2022-04-19 10:57:16 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,23 @@
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
+    let wrench = {
+        size: 14,
+        finish: "Zinc",
+        branch: "Mastercraft"
+    };
 
-    while (name != "Exit") 
-    {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
-    }
+    let computer = {
+        type: "Laptop",
+        brand: "HP",
+        isOn: false,
+        battery: 1.0,
+        powerOn: function() {
+            this.isOn = true;
+            this.battery -= 0.1;
+        },
+        powerOff: function() {
+            this.isOn = false;
+        }
+    };
 }
 
 
