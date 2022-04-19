9744ea738d80c77b51bdd9adf0f97d7719568f52
James Grieve <jgrieve@ualberta.ca>
2022-04-19 11:56:30 -0600

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,43 @@
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
-
-    while (name != "Exit") 
-    {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
+    class Computer {
+        constructor(type, brand) {
+            this.brand = brand;
+            switch(type.toUpperCase()) {
+                case "DESKTOP":
+                    this.batteryLevel = null;
+                    break;
+                case "LAPTOP":
+                case "TABLET":
+                    this.batteryLevel = 1.0;
+                    break;
+                default:
+                    throw "Unexpected value for 'type'!";
+                    break;
+            }
+            this.type = type.toUpperCase();
+        }
+        powerOn() {
+            this.isOn = true;
+            if (this.batteryLevel !== null) {
+                if (this.batteryLevel < 0.1)
+                {
+                    throw "Insufficient battery power!";
+                }
+                this.batteryLevel -= 0.1;
+            }
+        }
+        powerOff() {
+            this.isOn = false;
+        }
+        isOn = false;
     }
+
+    let desktop = new Computer("desktop", "EMachines");
+    let laptop = new Computer("laptop", "Dell");
+
+    output(JSON.stringify(laptop));
+    laptop.powerOn();    
+    output(JSON.stringify(laptop));
 }
 
 
