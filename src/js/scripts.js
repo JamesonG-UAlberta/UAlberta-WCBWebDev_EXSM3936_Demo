28d521d4544a255491d450ccc52bbf297f5393ab
James Grieve <jgrieve@ualberta.ca>
2022-04-19 09:38:30 -0600

    Anonymous object methods; Exceptions review;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,15 +1,43 @@
 async function main() {
-    let myFirstObject = {
-        firstName: "John",
-        lastName: "Doe",
-        age: 20,
-        isHappy: true,
-        fullName: function() {
-            return this.firstName + " " + this.lastName;
+    let pen = {
+        pointSize: "2mm",
+        pointType: "Felt",
+        housingColour: "Blue",
+        housingMaterial: "Plastic",
+        housingDiameter: 12,
+        housingLength: 60,
+        inkColour: "Blue",
+        inkType: "Ink",
+        inkViscosity: 0.7,
+        inkRefillable: true,
+        inkLevel: 0.85,
+        brand: "Sharpie",
+        hasCap: true,
+        retractable: true,
+        intact: true,
+        write: function(numChars) {
+            if (this.inkLevel*100 >= numChars) // If we have enough ink to write the characters.
+            {
+                this.inkLevel = (this.inkLevel-numChars/100).toFixed(2);
+            }
+            else
+            {
+                throw "Not enough ink!";
+            }
         }
     };
-    myFirstObject.lastName = "Test";
-    output(myFirstObject.fullName());
+
+    try {
+        output(pen.inkLevel); // 0.85
+        pen.write(20); // Success
+        output(pen.inkLevel); // 0.65
+        pen.write(70); // Require more ink than we have, throws exception.
+    }
+    catch (exception) // Catches exception.
+    {
+        output(exception); // Outputs exception.
+    }
+
 }
 
 
