e82bf706bd8c49b774be1d24e837570e9ca52f41
James Grieve <jgrieve@ualberta.ca>
2022-04-20 11:47:15 -0600

    Skeleton;

---
+++ src/js/scripts.js
@@ -0,0 +1,13 @@
+let input1 = document.querySelector("#input1");
+let input2 = document.querySelector("#input2");
+let input3 = document.querySelector("#input3");
+let startBtn = document.querySelector("#start");
+let stopBtn = document.querySelector("#stop");
+let inputs = [input1, input2, input3];
+
+startBtn.addEventListener("click", function() {
+    
+});
+stopBtn.addEventListener("click", function() {
+    
+});
