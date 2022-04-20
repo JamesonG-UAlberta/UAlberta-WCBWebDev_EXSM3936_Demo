be12965c58acb288047d2425703dd926a1d07500
James Grieve <jgrieve@ualberta.ca>
2022-04-20 09:28:54 -0600

    Anonymous object with integrated prompts;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,34 +1,21 @@
 async function main() {
     // Declare anonymous object.
     let clientHouse = {
-        clientFirstName: "",
-        clientLastName: "",
-        clientHomeAddress: "",
-        clientPhoneNumber: 0,
-        houseType: "",
-        houseSize: 0,
-        houseConstructionYear: 0,
-        houseSwimmingPool: false,
-        houseSmokeDetectors: false,
-        houseFireplace: false,
-        houseNearFireStation: false,
+        clientFirstName: await input("Please enter the client's first name: "),
+        clientLastName: await input("Please enter the client's last name: "),
+        clientHomeAddress: await input("Please enter the client's home address: "),
+        clientPhoneNumber: Number(await input("Please enter the client's phone number: ")),
+        houseType: await input("Please enter the house type: "),
+        houseSize: Number(await input("Please enter the house size (sq ft): ")),
+        houseConstructionYear: Number(await input("Please enter the house year of construction: ")),
+        houseSwimmingPool: JSON.parse(await input("Please enter whether the house has a swimming pool (true/false): ")),
+        houseSmokeDetectors: JSON.parse(await input("Please enter whether the house has at least one smoke detector (true/false): ")),
+        houseFireplace: JSON.parse(await input("Please enter whether the house has a fireplace (true/false): ")),
+        houseNearFireStation: JSON.parse(await input("Please enter whether the house is near (10km) a fire station (true/false): ")),
         fullName: function() {
             return this.clientFirstName + " " + this.clientLastName;
         }
     }
-
-    // Gather our information, convert to the appropriate type, store in the object.
-    clientHouse.clientFirstName = await input("Please enter the client's first name: ");
-    clientHouse.clientLastName = await input("Please enter the client's last name: ");
-    clientHouse.clientHomeAddress = await input("Please enter the client's home address: ");
-    clientHouse.clientPhoneNumber = Number(await input("Please enter the client's phone number: "));
-    clientHouse.houseType = await input("Please enter the house type: ");
-    clientHouse.houseSize = Number(await input("Please enter the house size (sq ft): "));
-    clientHouse.houseConstructionYear = Number(await input("Please enter the house year of construction: "));
-    clientHouse.houseSwimmingPool = JSON.parse(await input("Please enter whether the house has a swimming pool (true/false): "));
-    clientHouse.houseSmokeDetectors = JSON.parse(await input("Please enter whether the house has at least one smoke detector (true/false): "));
-    clientHouse.houseFireplace = JSON.parse(await input("Please enter whether the house has a fireplace (true/false): "));
-    clientHouse.houseNearFireStation = JSON.parse(await input("Please enter whether the house is near (10km) a fire station (true/false): "));
 
     // Stringify and output.
     output(JSON.stringify(clientHouse));
