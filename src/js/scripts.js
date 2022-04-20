85be28d3ee51ab37df7ece0e177490dacfa39654
James Grieve <jgrieve@ualberta.ca>
2022-04-20 10:43:42 -0600

    Added console snippets as comments;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,12 @@
+/*
+let paragraph = document.querySelector("#paragraph");
+let input = document.querySelector("#input");
+
+paragraph.innerText = "I'm going to magic this text into the page!";
+paragraph.innerHTML = "Test<br/>Test";
+paragraph.innerText = input.value;
+*/
+
 let button = document.querySelector("#button");
 
 button.addEventListener("click", function() {
