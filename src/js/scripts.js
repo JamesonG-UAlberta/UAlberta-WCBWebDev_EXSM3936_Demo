af80b467543f79b80f73668949cef8474e3053a4
James Grieve <jgrieve@ualberta.ca>
2022-04-20 09:38:34 -0600

    Classed object with integrated prompts;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,25 +1,45 @@
 async function main() {
-    // Declare anonymous object.
-    let clientHouse = {
-        clientFirstName: await input("Please enter the client's first name: "),
-        clientLastName: await input("Please enter the client's last name: "),
-        clientHomeAddress: await input("Please enter the client's home address: "),
-        clientPhoneNumber: Number(await input("Please enter the client's phone number: ")),
-        houseType: await input("Please enter the house type: "),
-        houseSize: Number(await input("Please enter the house size (sq ft): ")),
-        houseConstructionYear: Number(await input("Please enter the house year of construction: ")),
-        houseSwimmingPool: JSON.parse(await input("Please enter whether the house has a swimming pool (true/false): ")),
-        houseSmokeDetectors: JSON.parse(await input("Please enter whether the house has at least one smoke detector (true/false): ")),
-        houseFireplace: JSON.parse(await input("Please enter whether the house has a fireplace (true/false): ")),
-        houseNearFireStation: JSON.parse(await input("Please enter whether the house is near (10km) a fire station (true/false): ")),
-        fullName: function() {
-            return this.clientFirstName + " " + this.clientLastName;
+    class ClientHouse
+    {
+        // Expect everything as strings. If they're already bool/number, then great, but if not we can convert them to be safe.
+        constructor(clientFirstName, clientLastName, clientHomeAddress, clientPhoneNumber, houseType, houseSize, houseConstructionYear, houseSwimmingPool, houseSmokeDetectors, houseFireplace, houseNearFireStation)
+        {
+            this.clientFirstName = clientFirstName;
+            this.clientLastName = clientLastName;
+            this.clientHomeAddress = clientHomeAddress;
+            this.clientPhoneNumber = Number(clientPhoneNumber);
+            this.houseType = houseType;
+            this.houseSize = Number(houseSize);
+            this.houseConstructionYear = Number(houseConstructionYear);
+            this.houseSwimmingPool = JSON.parse(houseSwimmingPool);
+            this.houseSmokeDetectors = JSON.parse(houseSmokeDetectors);
+            this.houseFireplace = JSON.parse(houseFireplace);
+            this.houseNearFireStation = JSON.parse(houseNearFireStation);
+        }
+        fullName = function() {
+            return this.clientFirstName + " " + clientLastName;
         }
     }
 
+    let theClientHouse = new ClientHouse(
+        await input("Please enter the client's first name: "),
+        await input("Please enter the client's first name: "), 
+        await input("Please enter the client's home address: "),
+        await input("Please enter the client's phone number: "),
+        await input("Please enter the house type: "),
+        await input("Please enter the house size (sq ft): "),
+        await input("Please enter the house year of construction: "),
+        await input("Please enter whether the house has a swimming pool (true/false): "),
+        await input("Please enter whether the house has at least one smoke detector (true/false): "),
+        await input("Please enter whether the house has a fireplace (true/false): "),
+        await input("Please enter whether the house is near (10km) a fire station (true/false): ")
+    );
+
+
     // Stringify and output.
-    output(JSON.stringify(clientHouse));
+    output(JSON.stringify(theClientHouse));
     //{"clientFirstName":"Joe","clientLastName":"Smith","clientHomeAddress":"123 Cool St","clientPhoneNumber":7802345432,"houseType":"Duplex","houseSize":1200,"houseConstructionYear":2001,"houseSwimmingPool":false,"houseSmokeDetectors":true,"houseFireplace":true,"houseNearFireStation":true}
+
 }
 
 
