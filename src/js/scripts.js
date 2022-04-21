0ba6050a137e2f3b7a7b74c642716a46a21ac0da
James Grieve <jgrieve@ualberta.ca>
2022-04-21 10:45:15 -0600

    Day age;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,22 @@
 async function main() {
-    // This is where the code you're actually experimenting with goes.
-    
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
-
-    while (name != "Exit") 
-    {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
-    }
+    output(calcAge([1990, 1, 1]));
 }
 
+// Inputs: Birthdate (structured [year (number), month (number), day (number)])
+// Outputs: Age in Days
+function calcAge(birthdate)
+{
+    let dayAge;
 
+    let currentYear = new Date().getFullYear();
+    let currentMonth =  new Date().getMonth()+1;
+    let currentDay = new Date().getDate();
+
+    let yearDifference = currentYear - birthdate[0];
+    let monthDifference = currentMonth - birthdate[1];
+    let dayDifference = currentDay - birthdate[2];
+
+    dayAge = yearDifference * 365.25 + monthDifference * 30 + dayDifference;
+
+    return dayAge;
+}
