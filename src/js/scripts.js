5469203f16df88ad9cc2d7825423a221695eeeba
James Grieve <jgrieve@ualberta.ca>
2022-04-21 10:07:04 -0600

    Skeleton;

---
+++ src/js/scripts.js
@@ -0,0 +1,9 @@
+const gallery = docuemnt.querySelector("#gallery");
+const url = docuemnt.querySelector("#url");
+const goBtn = docuemnt.querySelector("#add");
+
+
+// e => {} is the same as function (e) {}
+goBtn.addEventListener("click", e => {
+    
+});
