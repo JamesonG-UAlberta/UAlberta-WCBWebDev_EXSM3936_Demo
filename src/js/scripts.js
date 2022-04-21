1074471e10ac31e2106b8c0e3b9532080b661e4e
James Grieve <jgrieve@ualberta.ca>
2022-04-21 09:21:30 -0600

    Initializing array with references;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,9 @@
-let input1 = document.querySelector("#fName");
-let input2 = document.querySelector("#lName");
-let input3 = document.querySelector("#age");
-let input4 = document.querySelector("#gender");
-let input5 = document.querySelector("#issue");
-let startBtn = document.querySelector("#submit");
-let stopBtn = document.querySelector("#clear");
+const submitBtn = document.querySelector("#submit");
+const clearBtn = document.querySelector("#clear");
 // Since the inputs are object references, we can use them to build an array of inputs.
-let inputs = [input1, input2, input3, input4, input5];
+const inputs = [document.querySelector("#fName"), document.querySelector("#lName"), document.querySelector("#age"), document.querySelector("#gender"), document.querySelector("#issue")];
 
-startBtn.addEventListener("click", function (e) {
+submitBtn.addEventListener("click", function (e) {
     e.preventDefault(); // Because our button is in a form, it automatically tries to submit the form when clicked. Note that you need to have "e" (or whatever identifier) specified as a parameter of the function in line 11.
     for (input of inputs) {
         if (input.value == "" && !input.classList.contains("alert")) { // If it's empty and not red 
@@ -16,7 +11,7 @@ startBtn.addEventListener("click", function (e) {
         }
     }
 });
-stopBtn.addEventListener("click", function (e) {
+clearBtn.addEventListener("click", function (e) {
     e.preventDefault(); // See line 12.
     for (input of inputs) {
         input.classList.remove("alert"); // Remove red.
