dbc59674b142e718bd85a6c230953b3d74340318
James Grieve <jgrieve@ualberta.ca>
2022-04-21 10:18:47 -0600

    Adding images;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,12 @@
-const gallery = docuemnt.querySelector("#gallery");
-const url = docuemnt.querySelector("#url");
-const goBtn = docuemnt.querySelector("#add");
+const gallery = document.querySelector("#gallery");
+const url = document.querySelector("#url");
+const goBtn = document.querySelector("#add");
 
 
 // e => {} is the same as function (e) {}
 goBtn.addEventListener("click", e => {
-    
+   let imageURL = url.value;
+   const newElement = document.createElement("img"); // When we create this element, it is not on the page because it hasn't been appended to anything that is currently on the page. It's just kind of floating in memory.
+   newElement.setAttribute("src", url.value);
+    gallery.appendChild(newElement);
 });
