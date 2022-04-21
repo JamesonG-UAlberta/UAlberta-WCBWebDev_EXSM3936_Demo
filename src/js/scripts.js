bd0bac17024df56e194cc0ddc52369c3ded3032f
James Grieve <jgrieve@ualberta.ca>
2022-04-21 11:39:01 -0600

    Clear button; Delete button;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,36 @@
 const gallery = document.querySelector("#gallery");
 const url = document.querySelector("#url");
-const goBtn = document.querySelector("#add");
+const addBtn = document.querySelector("#add");
+const clearBtn = document.querySelector("#clear");
 
 
 // e => {} is the same as function (e) {}
-goBtn.addEventListener("click", e => {
+addBtn.addEventListener("click", e => {
    let imageURL = url.value;
+
+   // -- Create the Image -- //
    const newElement = document.createElement("img"); // When we create this element, it is not on the page because it hasn't been appended to anything that is currently on the page. It's just kind of floating in memory.
    newElement.setAttribute("src", url.value);
-    gallery.appendChild(newElement);
+
+   // -- Create the Container -- //
+    const newElementContainer = document.createElement("div");
+    const newElementClose = document.createElement("a");
+    newElementClose.innerText = "X";
+    newElementClose.addEventListener("click", e => {
+        newElementContainer.remove();
+    });
+
+    // -- Create the Hierarchy -- //
+    newElementContainer.appendChild(newElementClose);
+    newElementContainer.appendChild(newElement);
+
+   // -- Add to Page -- //
+    gallery.appendChild(newElementContainer);
+});
+clearBtn.addEventListener("click", e => {
+    const imageList = document.querySelectorAll("#gallery > div");
+    for (image of imageList)
+    {
+        image.remove();
+    }
 });
