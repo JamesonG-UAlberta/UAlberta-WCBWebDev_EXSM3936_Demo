b3d809d824145c25d8b0b8102790e841bd4638e3
James Grieve <jgrieve@ualberta.ca>
2022-04-21 09:41:11 -0600

    Arrow notation;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,40 @@ const clearBtn = document.querySelector("#clear");
 // Since the inputs are object references, we can use them to build an array of inputs.
 const inputIDs = ["fName", "lName", "age", "gender", "issue"];
 // map() runs a method on each item in an array, and builds a new array (in the same order) based on the results of the method. Here we are returning a querySelector() based on the inputID, so we get an array of 5 elements based on our 5 strings.
-const inputs = inputIDs.map(function (inputID) { return document.querySelector("#"+inputID); });
+const inputs = inputIDs.map(inputID => document.querySelector("#"+inputID));
+
+/*
+-- Arrow Notation --
+Given a function:
+
+function (x)
+{
+    return x + 1;
+}
+
+You can express that using "arrow notation" to save space, like so:
+
+x => x+1
+
+So, more complex:
+
+function (x)
+{
+    return (Math.floor(x)*2)-1;
+}
+
+into
+
+x => (Math.floor(x)*2)-1
+
+THIS ONLY WORKS IF THE FUNCTION CAN BE EXPRESSED IN ONE STATEMENT. Otherwise you need to declare a code block:
+
+x => {
+    let variable = 5;
+    return x+variable; 
+}
+
+*/
 
 
 submitBtn.addEventListener("click", function (e) {
