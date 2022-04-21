659f51015a3ee8cdb13a4bb4f7b4b894b58e40da
James Grieve <jgrieve@ualberta.ca>
2022-04-21 11:14:17 -0600

    Hiding with one button;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,13 @@
 const paragraph = document.querySelector("p");
-const showBtn = document.querySelector("#show");
-const hideBtn = document.querySelector("#hide");
+const toggleBtn = document.querySelector("#hide-toggle");
 
-showBtn.addEventListener("click", e => {
-    paragraph.classList.remove("hidden");
-    hideBtn.classList.remove("hidden");
-    showBtn.classList.add("hidden");
-});
-hideBtn.addEventListener("click", e => {
-    paragraph.classList.add("hidden");
-    hideBtn.classList.add("hidden");
-    showBtn.classList.remove("hidden");
+toggleBtn.addEventListener("click", e => {
+    if (paragraph.classList.contains("hidden")) { // Paragraph is hidden?
+        paragraph.classList.remove("hidden");
+        toggleBtn.innerText = "Hide";
+    }
+    else {
+        paragraph.classList.add("hidden");
+        toggleBtn.innerText = "Show";
+    }
 });
