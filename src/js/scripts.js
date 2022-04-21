db67b6687afb6ca47ecc9faf7f3623816f2ee21e
James Grieve <jgrieve@ualberta.ca>
2022-04-21 09:30:35 -0600

    Solution using arr.map();

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,10 @@
 const submitBtn = document.querySelector("#submit");
 const clearBtn = document.querySelector("#clear");
 // Since the inputs are object references, we can use them to build an array of inputs.
-const inputs = [document.querySelector("#fName"), document.querySelector("#lName"), document.querySelector("#age"), document.querySelector("#gender"), document.querySelector("#issue")];
+const inputIDs = ["fName", "lName", "age", "gender", "issue"];
+// map() runs a method on each item in an array, and builds a new array (in the same order) based on the results of the method. Here we are returning a querySelector() based on the inputID, so we get an array of 5 elements based on our 5 strings.
+const inputs = inputIDs.map(function (inputID) { return document.querySelector("#"+inputID); });
+
 
 submitBtn.addEventListener("click", function (e) {
     e.preventDefault(); // Because our button is in a form, it automatically tries to submit the form when clicked. Note that you need to have "e" (or whatever identifier) specified as a parameter of the function in line 11.
