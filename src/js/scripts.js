ebf78afa10b1f83a76530add0261022d16966315
James Grieve <jgrieve@ualberta.ca>
2022-04-21 11:11:35 -0600

    Hiding with hidden buttons;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,8 +3,12 @@ const showBtn = document.querySelector("#show");
 const hideBtn = document.querySelector("#hide");
 
 showBtn.addEventListener("click", e => {
-
+    paragraph.classList.remove("hidden");
+    hideBtn.classList.remove("hidden");
+    showBtn.classList.add("hidden");
 });
 hideBtn.addEventListener("click", e => {
-
+    paragraph.classList.add("hidden");
+    hideBtn.classList.add("hidden");
+    showBtn.classList.remove("hidden");
 });
