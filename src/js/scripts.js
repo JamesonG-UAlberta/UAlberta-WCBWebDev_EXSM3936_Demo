b13c7ded349f85f39363ceed5eb6f41571c58a41
James Grieve <jgrieve@ualberta.ca>
2022-04-27 10:41:27 -0600

    Results of query on page.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,12 +4,19 @@ const button = document.querySelector("#fetch");
 button.addEventListener("click", e => {
     e.preventDefault;
 
-    fetch("https://api.aakhilv.me/fun/facts?num=10") // Specify your API endpoint here.
-    .then(response => response.json()) // Convert the response to JSON.
-    .then(data => { // In these braces, you have your JSON data.
+    fetch("https://api.aakhilv.me/fun/facts?num=1") // Specify your API endpoint here.
+        .then(response => response.json()) // Convert the response to JSON.
+        .then(data => { // In these braces, you have your JSON data.
 
-        // Put the JSON data in the console.
-        console.log(data);
-    });
+            // Put the JSON data in the console.
+            paragraph.innerText+=" "+data;
+
+            /* 
+                i++ (i = i + 1)
+                i += 2 (i = i + 2)
+
+                "Hello" += "World" ("HelloWorld")
+            */
+        });
     
 });
