b510b0d7ebea17ae3f323b52b3f8bbe7092b54e7
James Grieve <jgrieve@ualberta.ca>
2022-04-27 10:23:03 -0600

    Comments;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,9 +4,11 @@ const button = document.querySelector("#fetch");
 button.addEventListener("click", e => {
     e.preventDefault;
 
-    fetch("https://api.aakhilv.me/fun/facts?num=10")
-    .then(response => response.json())
-    .then(data => {
+    fetch("https://api.aakhilv.me/fun/facts?num=10") // Specify your API endpoint here.
+    .then(response => response.json()) // Convert the response to JSON.
+    .then(data => { // In these braces, you have your JSON data.
+
+        // Put the JSON data in the console.
         console.log(data);
     });
     
