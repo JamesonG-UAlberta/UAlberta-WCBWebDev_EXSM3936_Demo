3d94c0677327df9faa3de552714bd230cbdffcd2
James Grieve <jgrieve@ualberta.ca>
2022-04-27 09:31:59 -0600

    Skeleton;

---
+++ src/js/scripts.js
@@ -0,0 +1,8 @@
+const paragraph = document.querySelector("#fun-fact");
+const button = document.querySelector("#fetch");
+
+button.addEventListener("click", e => {
+    e.preventDefault;
+
+    
+});
