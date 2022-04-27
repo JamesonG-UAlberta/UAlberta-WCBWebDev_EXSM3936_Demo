ee64bb60ffa9fcf2f16e3c13b4951feaad0f5984
James Grieve <jgrieve@ualberta.ca>
2022-04-27 10:21:05 -0600

    Axios being annoying; Using fetch instead;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,5 +4,10 @@ const button = document.querySelector("#fetch");
 button.addEventListener("click", e => {
     e.preventDefault;
 
+    fetch("https://api.aakhilv.me/fun/facts?num=10")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
     
 });
