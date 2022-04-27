26e7972d99875bc511e148b820bacf2c45d03106
James Grieve <jgrieve@ualberta.ca>
2022-04-27 11:21:48 -0600

    Recursion demo;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,24 @@
 async function main() {
     // This is where the code you're actually experimenting with goes.
     
-    let prompt = "Please enter your name, or 'Exit' to quit: "
-    let name = await input(prompt);
+    printNumber(5);
 
-    while (name != "Exit") 
+}
+function printNumber(num) 
+{
+    if (num < 100)
     {
-        output("Hello, "+name+"!");
-        name = await input(prompt);
-    }
+        output(num+1);
+        printNumber(num+1);
+    }    
 }
 
-
+/*
+-Output 5+1.
+-Call itself with 6.
+-Output 6+1.
+-Call itself with 7.
+-Output 7+1.
+-Call itself with 8.
+...
+*/
